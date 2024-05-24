36d9426cda8315d8c7139e7c3d1f008d854d5bda
NaaShidaaAddo <nsaaddo@st.ug.edu.gh>
2024-05-24 23:26:29 +0000

    Increased the font size of the text to 24

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>My name is Naa Shidaa</Text>
+      <Text style={{ fontSize: 24 }}>My name is Naa Shidaa</Text>
       <StatusBar style="auto" />
     </View>
   );
